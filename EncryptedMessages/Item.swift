bc4a7307a744b4f457a7b09866c821b9047e346d
Tiago Do Couto <cs.couto@outlook.com>
2025-12-27 19:34:29 +0800

    Initial Commit

---
+++ EncryptedMessages/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  EncryptedMessages
+//
+//  Created by Tiago Do Couto on 12/27/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ EncryptedMessagesTests/EncryptedMessagesTests.swift
@@ -0,0 +1,17 @@
+//
+//  EncryptedMessagesTests.swift
+//  EncryptedMessagesTests
+//
+//  Created by Tiago Do Couto on 12/27/25.
+//
+
+import Testing
+@testable import EncryptedMessages
+
+struct EncryptedMessagesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
